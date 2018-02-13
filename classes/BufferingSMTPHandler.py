3f6f160e968431775f289af10c80d40ed6b4f100
fabriziomiano@gmail.com <fabriziomiano@gmail.com>
2018-02-13 13:49:58 +0100

    Add BufferingSMTPHandler class to send logs via mail

---
+++ classes/BufferingSMTPHandler.py
@@ -0,0 +1,40 @@
+import string, logging, logging.handlers
+
+
+class BufferingSMTPHandler(logging.handlers.BufferingHandler):
+    """ 
+    Child class of logging.BufferingHandler to send records via mail
+
+
+    """
+    def __init__(self, mailhost, fromaddr, toaddrs, subject, capacity):
+        logging.handlers.BufferingHandler.__init__(self, capacity)
+        self.mailhost = mailhost
+        self.mailport = None
+        self.fromaddr = fromaddr
+        self.toaddrs = toaddrs
+        self.subject = subject
+        self.setFormatter(logging.Formatter(
+            "%(asctime)s %(levelname)-5s %(message)s"))
+
+    def flush(self):
+        if len(self.buffer) > 0:
+            try:
+                import smtplib
+                port = self.mailport
+                if not port:
+                    port = smtplib.SMTP_PORT
+                smtp = smtplib.SMTP(self.mailhost, port)
+                msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (
+                    self.fromaddr,
+                    string.join(self.toaddrs, ","),
+                    self.subject)
+                for record in self.buffer:
+                    s = self.format(record)
+                    print s
+                    msg = msg + s + "\r\n"
+                smtp.sendmail(self.fromaddr, self.toaddrs, msg)
+                smtp.quit()
+            except:
+                self.handleError(None)  # no particular record
+            self.buffer = []
