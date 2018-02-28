4d36bef072cbc280eb3983f4957299f3b7364924
fabriziomiano@gmail.com <fabriziomiano@gmail.com>
2018-02-28 17:29:38 +0100

    Add new path for webpage and summary_file_path tuples to access summary files

--- settings/constants.py
+++ settings/constants.py
@@ -2,12 +2,14 @@ input_home = ['/eos/atlas/atlascerngroupdisk/trig-daq/ART' ]
 project_home = '/afs/cern.ch/user/f/fmiano/TWAT'
 archive_home = ['/afs/cern.ch/user/f/fmiano/TWAT/archive']
 summary_home = ['/afs/cern.ch/user/f/fmiano/TWAT/summary']
-web_home = ['/afs/cern.ch/user/f/fmiano/TWAT/webpage/']
+web_home = ['/afs/cern.ch/user/f/fmiano/TWAT/webpage']
+plots_home = ['/afs/cern.ch/user/f/fmiano/TWAT/plots']
 # Change to these values when the code will be on atrvshft@lxplus.cern.ch 
 # project_home = '/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring' 
 # archive_home = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/archive']
 # summary_home = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/summary']
 # web_home = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/webpage']
+# plots_home = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/plots']
 
 ART = ['ART']
 branches = ['*'] 
@@ -28,12 +30,6 @@ additional_files = ['AOD.pool.root.checkFile',
 ]
 
 input_files = triginfo_file + additional_files 
-
-# input_files = ['AOD.pool.root.checkFiletrigSize.txt',
-#                'AOD.pool.root.checkFile',
-#                'AOD.pool.root.checkFile0',
-#                '*_script_log'
-# ]
 archived_files = [input_file + '.gz' for input_file in input_files]
 
 input_path_structure = (input_home, branches, projects, platforms,
@@ -42,4 +38,19 @@ input_path_structure = (input_home, branches, projects, platforms,
 archive_path_structure = (archive_home, ART, year, month, day,
                           branches, projects, clock, platforms,
                           samples, archived_files)
+
+summary_file = ['trigger']
+
+trig_categories = ['Total', 'Bjet', 'Bphys', 'Calo',\
+                   'Combined', 'Cosmics', 'Egamma',\
+                   'HeavyIon','Jet','L1','MET','MinBias',\
+                   'Muon','Steer','Tau','Tracking']
+
+summary_files = [f + category + '.txt' for f in summary_file for category in trig_categories]
+
+
+summary_files_path_structure = (summary_home, ART, year, month, day,
+                          branches, projects, clock, platforms,
+                               samples, summary_files)
+
 
