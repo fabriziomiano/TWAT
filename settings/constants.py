18dcc733de150ca74774d9af9a95c6069ea4d229
fabriziomiano@gmail.com <fabriziomiano@gmail.com>
2018-05-11 17:33:19 +0200

    Move email parameters to config.py

--- settings/constants.py
+++ settings/constants.py
@@ -3,24 +3,14 @@ import datetime
 import time as time_module
 
 INPUT_HOME = ['/eos/atlas/atlascerngroupdisk/trig-daq/ART']
-# PROJECT_HOME = '/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring'
-PROJECT_HOME = '/afs/cern.ch/user/f/fmiano/fabriziomiano/TWAT'
-# ARCHIVE_HOME = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/archive']
-ARCHIVE_HOME = ['/afs/cern.ch/user/f/fmiano/fabriziomiano/TWAT/archive']
+PROJECT_HOME = '/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring'
+ARCHIVE_HOME = ['/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/archive']
 WWW_HOME = '/eos/user/a/atrvshft/www/TriggerEDMSizeMonitoring/webpage'
-
-MAILHOST = 'localhost'
-FROM = 'atrvshft@lxplus.cern.ch'
-TO = ['fabriziomiano@gmail.com']
-SUBJECT = 'EDM Monitoring alert'
 
 LOG_DIRECTORY = "logs"
 LOG_FILE_NAME = os.path.join(
     LOG_DIRECTORY,
     "archive_" + time_module.strftime('%Y-%m-%d') + ".out")
-
-RANGE_ACCEPTED = 0.05
-REFS_PATH = os.path.join(PROJECT_HOME, 'edm.json')
 
 TODAY = datetime.datetime.now()
 WEEKDAY = TODAY.strftime("%w")
