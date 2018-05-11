42bb6d8b5154234920e6eae9d9085a62cd7885c2
fabriziomiano@gmail.com <fabriziomiano@gmail.com>
2018-05-11 17:31:53 +0200

    Change email address for daily email notification

--- settings/config.py
+++ settings/config.py
@@ -1,6 +1,11 @@
+"""
+Use this area to configure as you wish
+
+"""
+
 MAILHOST = 'localhost'
 FROM = 'atrvshft@lxplus.cern.ch'
-TO = ['fabrizio.miano@cern.ch']
+TO = ['atlas-trigger-edm@cern.ch']
 SUBJECT = 'EDM Monitoring alert'
 
 RANGE_ACCEPTED = 0.05
